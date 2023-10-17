984dd3af100b3f6134764b9636e61f7ff5ba282a
UnderSaga <undersaga@mail.ru>
2023-10-17 17:16:23 +0300

    Initial commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,33 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+@Epic("Первая группа")
+public class Test1 {
+
+    Steps step = new Steps();
+
+    String password;
+
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("Генерируем пароль...");
+        password = Main.generateRandomPassword(10);
+    }
+
+    @Test(description = "Первый тест")
+    @Description("Проверка валидности пароля")
+    public void testDesc() {
+        step.lengthTest(password);
+        step.digitTest(password);
+        step.specTest(password);
+    }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("Делаем что-то после тестов");
+    }
+
+}
